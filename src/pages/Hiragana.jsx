972936b945ddd56df0c054a14d07219faed3f3a2
Maxelone <maxelone@yandex.ru>
2025-04-19 14:39:05 +0400

    Test scroll indicator

--- src/pages/Hiragana.jsx
+++ src/pages/Hiragana.jsx
@@ -1,12 +1,37 @@
 import { Footer } from "@/widgets/Footer";
 import { Header } from "@/widgets/Header";
 
+import { motion, useSpring, useScroll } from "motion/react";
+
 export const Hiragana = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 40,
+    restDelta: 0.001,
+  });
+
   return (
     <>
       <Header />
 
-      <h1 className="text-center text-2xl font-bold px-6 mt-2">Хирагана</h1>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX,
+          position: "fixed",
+          top: 90,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+          backgroundColor: "#ff0088",
+        }}
+      />
+
+      <h1 className="px-6 mt-2 mb-[10000px] text-2xl font-bold text-center">
+        Хирагана
+      </h1>
 
       <Footer />
     </>
