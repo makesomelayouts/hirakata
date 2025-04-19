436282fd604dc3e5ab6f406bb62c593154531b6f
Maxelone <maxelone@yandex.ru>
2025-04-19 16:39:00 +0400

    Change h1 to full page

--- src/pages/Katakana.jsx
+++ src/pages/Katakana.jsx
@@ -1,12 +1,37 @@
 import { Footer } from "@/widgets/Footer";
 import { Header } from "@/widgets/Header";
 
+import { motion, useSpring, useScroll } from "motion/react";
+
 export const Katakana = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <>
       <Header />
-      <h1 className="text-center text-2xl font-bold px-6 mt-2">Катакана</h1>
-      <Footer />
+
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX,
+          position: "fixed",
+          top: 90,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+          backgroundColor: "#EBAAC1",
+        }}
+      />
+
+      <h1 className="px-6 mt-2 pb-[10000px] text-2xl font-bold text-center">
+        Катакана
+      </h1>
     </>
   );
 };
