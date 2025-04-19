c682bffbba99b29527624d49167ebd83720ea5a6
makesomelayouts <makesomelayouts@gmail.com>
2025-04-19 14:04:39 +0400

    Style active link

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -1,25 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = () => {
+  let location = useLocation();
+
   return (
     <header className="p-6">
       <div className="flex items-center justify-center gap-5">
         <Link
-          className="font-bold text-white transition border-b border-transparent duration-400 font-body hover:text-gray-200 hover:border-gray-300"
+          className={`${
+            location.pathname === "/"
+              ? "bg-[#5B3569] px-6 py-2 rounded-full"
+              : ""
+          } font-bold text-white transition border-b-2 md:text-xl duration-400 font-body border-transparent hover:text-gray-200 hover:border-gray-300`}
           to="/"
         >
           Главная
         </Link>
 
         <Link
-          className="font-bold text-white transition border-b border-transparent duration-400 font-body hover:text-gray-200 hover:border-gray-300"
+          className={`${
+            location.pathname === "/hiragana"
+              ? "bg-[#5B3569] px-6 py-2 rounded-full"
+              : ""
+          } font-bold text-white transition border-b-2 md:text-xl duration-400 font-body border-transparent hover:text-gray-200 hover:border-gray-300`}
           to="/hiragana"
         >
           Хирагана
         </Link>
 
         <Link
-          className="font-bold text-white transition border-b border-transparent duration-400 font-body hover:text-gray-200 hover:border-gray-300"
+          className={`${
+            location.pathname === "/katakana"
+              ? "bg-[#5B3569] px-6 py-2 rounded-full"
+              : ""
+          } font-bold text-white transition border-b-2 md:text-xl duration-400 font-body border-transparent hover:text-gray-200 hover:border-gray-300`}
           to="/katakana"
         >
           Катакана
