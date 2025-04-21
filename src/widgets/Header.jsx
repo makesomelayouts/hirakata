b64de07232f7be0e545792607fe3c9b14645f06a
Maxelone <maxelone@yandex.ru>
2025-04-21 22:16:50 +0300

    Update shadow

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="relative z-10 p-6">
       <img
-        src="/src/assets/Затемнение под шапку.png"
+        src="https://github.com/maxelonej/hirakata-website/blob/main/src/assets/%D0%97%D0%B0%D1%82%D0%B5%D0%BC%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BF%D0%BE%D0%B4%20%D1%88%D0%B0%D0%BF%D0%BA%D1%83.png?raw=true"
         className="absolute top-0 left-0 w-full -z-20 h-64 pointer-events-none"
       />
 
