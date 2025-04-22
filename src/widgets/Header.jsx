1fe6ec2f27ad45cbb90a4cdfb63b85f4314561e8
makesomelayouts <makesomelayouts@gmail.com>
2025-04-22 15:01:15 +0400

    Add new links: criteria and resources

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -44,6 +44,28 @@ export const Header = () => {
         >
           Катакана
         </TransitionLink>
+
+        <TransitionLink
+          className={`${
+            location.pathname === "/criteria"
+              ? "bg-[#5B3569] px-6 py-2 rounded-full"
+              : ""
+          } font-bold text-white transition border-b-2 md:text-xl duration-400 font-body border-transparent hover:text-gray-200 hover:border-gray-300`}
+          to="/criteria"
+        >
+          Критерии оценивания
+        </TransitionLink>
+
+        <TransitionLink
+          className={`${
+            location.pathname === "/resources"
+              ? "bg-[#5B3569] px-6 py-2 rounded-full"
+              : ""
+          } font-bold text-white transition border-b-2 md:text-xl duration-400 font-body border-transparent hover:text-gray-200 hover:border-gray-300`}
+          to="/resources"
+        >
+          Полезные ресурсы
+        </TransitionLink>
       </div>
     </header>
   );
