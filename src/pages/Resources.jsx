6c99a4202ad8e9dc1bc93e2abd8ce5301551c231
Maxelone <maxelone@yandex.ru>
2025-04-22 15:00:30 +0400

    Add criteria and resources pages

---
+++ src/pages/Resources.jsx
@@ -0,0 +1,26 @@
+import { Footer } from "@/widgets/Footer";
+import { Header } from "@/widgets/Header";
+import { motion } from "framer-motion";
+
+export const Resources = () => {
+  return (
+    <motion.div
+      className="min-h-screen"
+      style={{
+        background:
+          "linear-gradient(179.92deg, #C06BC9 -52.19%, #EBAAC1 61.17%)",
+      }}
+      initial="hidden"
+      animate="visible"
+    >
+      <Header />
+
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-center font-header text-5xl">Полезные ресурсы</h1>
+        <div className="mt-3.5 bg-[#5D4170] h-2.5 w-full rounded-full"></div>
+      </div>
+
+      <Footer />
+    </motion.div>
+  );
+};
